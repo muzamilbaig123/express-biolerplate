b6a3eb8499db1484fa25c877362bb890d96bafea
muzamilbaig123 <muzamilbaig24@gmail.com>
2024-12-27 09:47:10 -0800

    final biolerplate

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/users.js';
-
+import express from "express";
 const app = express();
+import homepage from "./routes/index.js"
 
-// view engine setup
-app.set('views', path.join(process.cwd(), 'views'));
-app.set('view engine', 'ejs');
+const port = 3000;
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(process.cwd(), 'public')));
+app.set("view engine", "ejs");
+app.use(express.static("./public"))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
-});
+    next()
+})
 
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use("/", homepage);
 
-export default app;
+
+app.listen(port)
